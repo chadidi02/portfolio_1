1f0b04f175c83d70647a78d1e66cbbfb86bf8f8e
Souhaib Chadidi <souhaibchadidi02@gmail.com>
2020-08-21 22:15:42 +0100

    haha

--- src/app.js
+++ src/app.js
@@ -1,34 +1,5 @@
-// const axios = require("axios");
 import axios from "axios";
 import "regenerator-runtime/runtime.js";
-
-// document.getElementById("post").addEventListener("click", addTodo);
-// axios
-// 	.get("https://www.fevrok.dev/api/v1/portfolio/chadidi/?api_key=du4iwvio46")
-// 	.then(function (response) {
-// 		const first_name = response.data.portfolio.user.first_name;
-// 		const last_name = response.data.portfolio.user.last_name;
-
-// 		// let last_name = response.data.portfolio.user.last_name;
-// 		let about = response.data.portfolio.user.about;
-
-// 		// let location = response.data.portfolio.user.location;
-
-// 		document.getElementById("first_name").innerHTML = first_name;
-// 		document.getElementById("last_name").innerHTML = last_name;
-
-// 		// document.getElementById("last_name").innerHTML = last_name;
-
-// 		document.getElementById("about").innerHTML = about;
-// 		document.getElementById("location").innerHTML = location;
-
-// 		console.log(response);
-
-// 		// console.log(response.status);
-// 		// console.log(response.statusText);
-// 		// console.log(response.headers);
-// 		// console.log(response.config);
-// 	});
 
 async function getUser(username) {
 	const {
